a540e070f3a92ace6afd05bbd59293782d92a398
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/environment/query/get_home.c

---
+++ src/environment/query/get_home.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** get
+** File description:
+** home
+*/
+
+#include "minishell.h"
+
+char *get_home(env_t *stock_env)
+{
+    for (env_t *tmp = stock_env; tmp; tmp = tmp->next) {
+        if (my_strcmp(tmp->key, "HOME") == 0 && tmp->value != NULL)
+            return tmp->value;
+    }
+    return NULL;
+}
