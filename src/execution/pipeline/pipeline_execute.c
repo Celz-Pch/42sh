444a0217c0b865ce175b91a1e9f192df05ccadaa
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/execution/pipeline/pipeline_execute.c

---
+++ src/execution/pipeline/pipeline_execute.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** execute
+** File description:
+** pipeline
+*/
+
+#include "minishell.h"
+
+static int run_pipeline_segments(pipeline_state_t *state, int count)
+{
+    for (int i = 0; i < count; i++)
+        if (push_segment(state, i))
+            return close_and_free(state);
+    return 0;
+}
+
+int execute_pipeline(main_t *stock_main, char *command)
+{
+    pipeline_state_t state;
+    int count = init_pipeline_state(&state, stock_main, command);
+
+    if (count < 0)
+        return 1;
+    if (run_pipeline_segments(&state, count))
+        return 1;
+    return finalize_pipeline(&state);
+}
