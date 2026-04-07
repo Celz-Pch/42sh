88cf4e6baee467148e4013353b6437c62d830e40
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/utils/strings/my_strcmp.c

---
+++ src/utils/strings/my_strcmp.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** my_strcmp
+** File description:
+** my_strcmp
+*/
+
+#include "minishell.h"
+
+int my_strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    if (s1 == NULL || s2 == NULL)
+        return -1;
+    while (s1[i] == s2[i]) {
+        if (s1[i] == '\0')
+            return 0;
+        i++;
+    }
+    return s1[i] - s2[i];
+}
