1c39284cc562d6d80ea40baea8967ab6e5d6b07c
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-14 16:55:41 +0200

    fix: check alloc on repeat & exec all arg on repeat

--- src/builtins/repeat/repeat.c
+++ src/builtins/repeat/repeat.c
@@ -14,15 +14,17 @@
 
 int repeat(main_t *main_stock, command_ctx_t *ctx)
 {
-    int nbr = atoi(ctx->arg_command[0]);
+    int nbr = 0;
     int value = 0;
     command_ctx_t repeated_ctx = *ctx;
 
+    if (ctx->arg_command != NULL && ctx->arg_command[0] != NULL)
+        nbr = atoi(ctx->arg_command[0]);
     if (nbr == 0)
         return SUCCESS;
     repeated_ctx.command = ctx->arg_command[1];
     repeated_ctx.argv = &ctx->arg_command[1];
-    repeated_ctx.arg_command = &ctx->arg_command[2];
+    repeated_ctx.arg_command = ctx->arg_command + 2;
     for (int i = 0; i < nbr; i++)
         value = exec_any(main_stock, &repeated_ctx);
     return value;
