4ef99b4df030969665c410b04f34dff0a9240fc5
czp <celenzo.peuch@epitech.eu>
2026-04-16 13:16:18 +0200

    feat: which error cases added

--- src/builtins/fs/my_which.c
+++ src/builtins/fs/my_which.c
@@ -12,10 +12,20 @@ int builtin_which(main_t *main_stock, command_ctx_t *ctx)
     int i = 0;
     char *bin_place = NULL;
 
+    if (ctx->arg_command[0] == NULL) {
+        printf("which: Too few arguments.\n");
+        return 84;
+    }
     for (; main_stock->path[i] != NULL; i++) {
         bin_place = check_bin(ctx->arg_command[0], main_stock->path[i]);
-        if (bin_place != NULL)
+        if (bin_place != NULL) {
             printf("%s\n", bin_place);
+            break;
+        }
+    }
+    if (bin_place == NULL) {
+        printf("%s: Command not found.\n", ctx->arg_command[0]);
+        return 84;
     }
     return 0;
 }
