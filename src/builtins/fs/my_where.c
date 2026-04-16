6588d835b748a11c69b1fe76b2efcd3cc2e16d9b
czp <celenzo.peuch@epitech.eu>
2026-04-16 13:15:49 +0200

    feat: where error cases added

--- src/builtins/fs/my_where.c
+++ src/builtins/fs/my_where.c
@@ -12,6 +12,10 @@ int builtin_where(main_t *main_stock, command_ctx_t *ctx)
     int i = 0;
     char *bin_place = NULL;
 
+    if (ctx->arg_command[0] == NULL) {
+        printf("where: Too few arguments.\n");
+        return 84;
+    }
     for (; main_stock->path[i] != NULL; i++) {
         bin_place = check_bin(ctx->arg_command[0], main_stock->path[i]);
         if (bin_place != NULL)
